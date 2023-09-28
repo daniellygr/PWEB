e6fd5ea0d4f5231cf37e7cfa495f06c24565e6b4
Danielly Gallão Rocha <112280193+daniellygr@users.noreply.github.com>
2023-09-27 22:10:14 -0300

    Add files via upload

---
+++ Atividade8/jokenpo.js
@@ -0,0 +1,65 @@
+﻿function selecionarOpcao(jogada) {
+
+
+    if (jogada === "pedra") {
+      
+        alert("Você escolheu " + jogada + "!");
+        var computador = Math.floor((Math.random() * 3));
+        var pedra = Math.floor((Math.random() * 3));
+
+        if (pedra > computador) {
+            alert("O computador escolheu tesoura. Você ganhou!");
+
+        }
+        else if (pedra < computador) {
+            alert("O computador escolheu papel. Você perdeu!");
+
+        }
+        else {
+            alert("O computador escolheu pedra. Empate!");
+        }
+
+    }
+
+    else if (jogada === "papel") {
+
+        alert("Você escolheu " + jogada + "!");
+        var computador = Math.floor((Math.random() * 3));
+        var papel = Math.floor((Math.random() * 3));
+
+        if (papel > computador) {
+            alert("O computador escolheu pedra. Você ganhou!");
+
+        }
+        else if (papel < computador) {
+            alert("O computador escolheu tesoura. Você perdeu!");
+
+        }
+        else
+        {
+            alert("O computador escolheu papel. Empate!");
+        }
+
+    }
+
+    else {
+
+        alert("Você escolheu " + jogada + "!");
+        var computador = Math.floor((Math.random() * 3));
+        var tesoura = Math.floor((Math.random() * 3));
+
+        if (tesoura > computador) {
+            alert("O computador escolheu papel. Você ganhou!");
+
+        }
+        else if (tesoura < computador) {
+            alert("O computador escolheu pedra. Você perdeu!");
+
+        }
+        else {
+            alert("O computador escolheu tesoura. Empate!");
+        }
+
+    }
+
+}
